b5a5fb5a72d4e251d480e6a3de642f34657235dc
Iso-Doss <dossou.israel48@gmail.com>
2016-10-24 16:08:36 +0100

    suppression des dossier lourdes

--- app/config/config.php
+++ app/config/config.php
@@ -6,7 +6,7 @@ return new \Phalcon\Config(array(
         'host'        => 'localhost',
         'username'    => 'root',
         'password'    => '',
-        'dbname'      => 'flv79',
+        'dbname'      => 'fvl79',
     ),
     'application' => array(
         'controllersDir' => __DIR__ . '/../../app/controllers/',
@@ -15,6 +15,6 @@ return new \Phalcon\Config(array(
         'pluginsDir'     => __DIR__ . '/../../app/plugins/',
         'libraryDir'     => __DIR__ . '/../../app/library/',
         'cacheDir'       => __DIR__ . '/../../app/cache/',
-        'baseUri'        => 'http://localhost/FLV79/',
+        'baseUri'        => 'http://localhost/FVL79/',
     )
 ));
--- app/controllers/FormationsController.php
+++ app/controllers/FormationsController.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,11 +15,11 @@ class FormationsController extends BaseController {
     public function initialize() {
         $this->view->setMainView('main');
     }
-    
+
     public function indexAction() {
         
     }
-    
+
     public function listAction() {
         
     }
@@ -28,8 +27,27 @@ class FormationsController extends BaseController {
     public function descriptionAction() {
         
     }
-    
+
     public function ajoutAction() {
-        
-    }
+        if (isset($_POST['libelle_formation']) AND isset($_POST['description_formation']) AND isset($_POST['cout_formation']) ) {
+            try {
+                $bdd = new PDO('mysql:host=localhost;dbname=fvl79', 'root', '');
+            } catch (Exception $e) {
+                die('Erreur : ' . $e->getMessage());
+            }
+
+            $req = $bdd->prepare('INSERT INTO client (codeclient, nom, prenoms, datenaissance, email, ville, telephone) VALUES(?, ?, ?, ?, ?, ?, ?)');
+            $req->execute(array($_POST['codeclient'], $_POST['nom'], $_POST['prenoms'], $date, $_POST['email'], $_POST['ville'], $_POST['telephone']));
+
+            echo "Formation créée";
+            header('Location: index1.php?pg=clients');
+        } else {
+            echo "echec! reessayez";
+        }
+        ?>
+}
+
+public function creationAction() {
+
+}
 }
